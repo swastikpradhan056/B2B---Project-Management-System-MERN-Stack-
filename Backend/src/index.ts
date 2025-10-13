8078e518005ecbd6973079352b4b94e65078fd43
Swastik Raj Pradhan Wanta <swastikpradhan056@gmail.com>
2025-10-13 14:54:47 +0545

    Implement join workspace functionality with invite code validation and member role assignment

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -19,6 +19,7 @@ import authRoutes from "./routes/auth.route";
 import userRoutes from "./routes/user.route";
 import isAuthenticated from "./middlewares/isAuthenticated.middleware";
 import workspaceRoutes from "./routes/workspace.route";
+import memberRoutes from "./routes/member.route";
 
 const app = express();
 const BASE_PATH = config.BASE_PATH;
@@ -78,6 +79,7 @@ app.get("/" /* ... */);
 app.use(`${BASE_PATH}/auth`, authRoutes);
 app.use(`${BASE_PATH}/user`, isAuthenticated, userRoutes);
 app.use(`${BASE_PATH}/workspace`, isAuthenticated, workspaceRoutes);
+app.use(`${BASE_PATH}/member`, isAuthenticated, memberRoutes);
 app.use(errorHandler);
 
 app.listen(config.PORT, async () => {
--- Backend/src/services/member.service.ts
+++ Backend/src/services/member.service.ts
@@ -1,7 +1,13 @@
 import { ErrorCodeEnum } from "../enums/error-code.enum";
+import { Roles } from "../enums/role.enum";
 import MemberModel from "../models/member.model";
+import RoleModel from "../models/roles-permission.model";
 import WorkspaceModel from "../models/workspace.model";
-import { NotFoundException, UnauthorizedException } from "../utils/appError";
+import {
+  BadRequestException,
+  NotFoundException,
+  UnauthorizedException,
+} from "../utils/appError";
 
 /**
  * The function `getMemberRoleInWorkspace` retrieves the role of a member in a workspace based on the
@@ -39,4 +45,45 @@ export const getMemberRoleInWorkspace = async (
   const roleName = member.role?.name;
 
   return { role: roleName };
+};
+
+export const joinWorkspaceByInviteService = async (
+  userId: string,
+  inviteCode: string
+) => {
+  // Find workspace by invite code
+  const workspace = await WorkspaceModel.findOne({ inviteCode });
+  if (!workspace) {
+    throw new NotFoundException("Invalid invite code or workspace not found");
+  }
+
+  // check if user is already a member
+  const existingMember = await MemberModel.findOne({
+    userId,
+    workspaceId: workspace._id,
+  }).exec();
+
+  if (existingMember) {
+    throw new BadRequestException(
+      "You are already a member of this workspace."
+    );
+  }
+
+  const role = await RoleModel.findOne({
+    name: Roles.MEMBER,
+  });
+
+  if (!role) {
+    throw new NotFoundException("Role not found");
+  }
+
+  // Add user to workspace as a member
+  const newMember = new MemberModel({
+    userId,
+    workspaceId: workspace._id,
+    role: role._id,
+  });
+  await newMember.save();
+
+  return { workspaceId: workspace._id, role: role.name };
 };
